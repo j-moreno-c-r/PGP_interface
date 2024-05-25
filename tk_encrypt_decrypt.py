4045512651aa1e13cf7b62835995b63da3cdcd06
j-moreno-c-r <joaomorenocruzrosa@gmail.com>
2024-05-24 21:01:36 -0300

    qr code for encrypted message

--- tk_encrypt_decrypt.py
+++ tk_encrypt_decrypt.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import base64
 from encrypt_decrypt import encrypt, decrypt
+from qr_keys_output import create_qr_code
 
 def create_encrypt_interface():
     window = tk.Tk()
@@ -50,6 +51,8 @@ def create_decrypt_interface():
     encrypted_message_entry = tk.Entry(window, font=("Courier", 14), bg='black', fg='green2', insertbackground='green2')
     decrypted_label = tk.Label(window, text="Decrypted message:", font=("Courier", 14), bg='black', fg='green2')
     decrypted_message_text = tk.Text(window, font=("Courier", 14), bg='black', fg='green2', wrap='word')
+    button = tk.Button(window, text="Qr encripted message", command=lambda: create_qr_code(str(encrypted_message_entry),"Encrypted Message", size=3), font=("Courier", 14), bg='black', fg='green2')
+
 
     decrypted_scrollbar = tk.Scrollbar(window, command=decrypted_message_text.yview)
     decrypted_message_text['yscrollcommand'] = decrypted_scrollbar.set
@@ -69,13 +72,16 @@ def create_decrypt_interface():
     window.grid_columnconfigure(0, weight=1)
     window.grid_rowconfigure((4, 7), weight=1)
 
+    
     decrypt_button = tk.Button(window, text="Decrypt", command=decrypt_message, font=("Courier", 14), bg='black', fg='green2')
     encrypted_message_entry = tk.Text(window, height=10, width=50, font=("Courier", 14), bg='black', fg='green2', highlightbackground='red', highlightcolor='red', highlightthickness=2)
     encrypted_label.grid(row=0, column=0, sticky='w', padx=10, pady=10)
+    button.grid(row=0, column=1, sticky='w', padx=10, pady=10)
     encrypted_message_entry.grid(row=1, column=0, sticky='ew', padx=10, pady=10)
     decrypt_button.grid(row=2, column=0, sticky='ew', padx=10, pady=10)
     decrypted_label.grid(row=3, column=0, sticky='w', padx=10, pady=10)
     decrypted_message_text.grid(row=4, column=0, sticky='nsew', padx=10, pady=10)
     decrypted_scrollbar.grid(row=4, column=1, sticky='ns', padx=10, pady=10)
+    
 
     window.mainloop()
